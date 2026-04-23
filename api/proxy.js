a17b3039ea28bc4f993a49a63921e20140041589
Arpit Gaur <gaur68124@gmail.com>
2026-04-23 13:27:14 +0530

    Fix API function for Vercel compatibility

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -27,4 +27,4 @@ export default async function handler(req, res) {
     console.error('Proxy error:', error);
     res.status(500).json({ error: 'Proxy error', message: error.message });
   }
-}
+};
